17cadde4a1fe35a3abab577f8ff821803cc6fb06
jugesh600 <legendjugesh600@gmail.com>
2025-09-28 15:15:37 +0530

    final change

--- src/Confug/index.js
+++ src/Confug/index.js
@@ -1,5 +1,5 @@
-import Mainlayout from "../Components/layout/Mainlayout.jsx";
-import Footer from "../Components/footer/Footer.jsx";
+import Mainlayout from "../components/layout/Mainlayout.jsx";
+import Footer from "../components/footer/Footer.jsx";
 import Home from "../pages/home/Home.jsx";
 import About from "../pages/about/About.jsx";
 import Contact from "../pages/Contact/contact.jsx";
