341bd7c7cc23349c9f1af4a48abe2ee525e1c3d6
jugesh600 <legendjugesh600@gmail.com>
2025-09-28 15:21:40 +0530

    speling change

---
+++ src/Config/index.js
@@ -0,0 +1,8 @@
+import Mainlayout from "../components/Layout/Mainlayout.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Home from "../pages/Home/Home.jsx";
+import About from "../pages/About/About.jsx";
+import Contact from "../pages/Contact/Contact.jsx";
+import Project from "../pages/Project/Project.jsx";
+import Skills from "../pages/Skill/skills.jsx";
+export {  Footer, Home, About, Project, Contact , Mainlayout, Skills};
