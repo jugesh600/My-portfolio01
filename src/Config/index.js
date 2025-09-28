a0487e296ba1939c21d608c48482e04c092022bc
jugesh600 <legendjugesh600@gmail.com>
2025-09-28 15:42:50 +0530

    04change

--- src/Config/index.js
+++ src/Config/index.js
@@ -1,5 +1,5 @@
-import MainLayout from "../Components/Layout/MainLayout.jsx";
-import Footer from "../Components/Footer/Footer.jsx";
+import MainLayout from "../components/Layout/MainLayout.jsx";
+import Footer from "../components/Footer/Footer.jsx";
 import Home from "../pages/Home/Home.jsx";
 import About from "../pages/About/About.jsx";
 import Contact from "../pages/Contact/Contact.jsx";
